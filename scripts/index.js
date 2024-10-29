8d57a4ec443a93295d05b5cf4d15263ce3ce0248
Raquel <raquelosenoret@gmail.com>
2024-10-29 13:37:20 -0600

    Project 4 Stage 1. An Array of Elements

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initalCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://images.unsplash.com/photo-1498429089284-41f8cf3ffd39?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHlvc2VtaXRlJTIwbmF0aW9uYWwlMjBwYXJrfGVufDB8fDB8fHww",
+    },
+    {
+        name: "Lake Louise",
+        link: "https://images.unsplash.com/photo-1565477728049-ee08e1456755?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fGxha2UlMjBsb3Vpc2V8ZW58MHx8MHx8fDA%3D",
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://images.unsplash.com/photo-1558737429-a0791ed130b6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhbGQlMjBtb3VudGFpbnxlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+        name: "Latemar",
+        link: "https://images.unsplash.com/photo-1536431311719-398b6704d4cc?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://images.unsplash.com/photo-1591368619421-33cc6e258886?q=80&w=1635&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://images.unsplash.com/photo-1685207929618-34b15187e3c8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFnbyUyMGRlJTIwYnJhaXNlfGVufDB8fDB8fHww",
+    },
+];
+
+console.log(initalCards);
