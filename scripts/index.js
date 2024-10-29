c0a8cca7deff8b0c49d25b6926a2d9ea200becff
Raquel <raquelosenoret@gmail.com>
2024-10-29 13:52:22 -0600

    second corrections on array of elements

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [ 
     {
         name: "Yosemite Valley",
-        link: "https://images.unsplash.com/photo-1498429089284-41f8cf3ffd39?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHlvc2VtaXRlJTIwbmF0aW9uYWwlMjBwYXJrfGVufDB8fDB8fHww",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
     },
     {
         name: "Lake Louise",
-        link: "https://images.unsplash.com/photo-1565477728049-ee08e1456755?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fGxha2UlMjBsb3Vpc2V8ZW58MHx8MHx8fDA%3D",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
     },
     {
         name: "Bald Mountains",
-        link: "https://images.unsplash.com/photo-1558737429-a0791ed130b6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhbGQlMjBtb3VudGFpbnxlbnwwfHwwfHx8MA%3D%3D",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
     },
     {
         name: "Latemar",
-        link: "https://images.unsplash.com/photo-1536431311719-398b6704d4cc?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
     },
     {
         name: "Vanoise National Park",
-        link: "https://images.unsplash.com/photo-1591368619421-33cc6e258886?q=80&w=1635&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
     },
     {
         name: "Lago di Braies",
-        link: "https://images.unsplash.com/photo-1685207929618-34b15187e3c8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFnbyUyMGRlJTIwYnJhaXNlfGVufDB8fDB8fHww",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
     },
 ];
 
